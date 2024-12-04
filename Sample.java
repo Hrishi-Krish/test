edaa09e6fb0680a56332172ac803957ea223414a
Hrishikesh Krishnan <hrishasur@gmail.com>
2024-12-04 16:31:15 +0530

    Another revert successfully done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,6 @@
 public class Sample {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Goodbye World!");
-    System.out.println("Hello Again World!");
+    System.out.println("Bye World!");
   }
 }
